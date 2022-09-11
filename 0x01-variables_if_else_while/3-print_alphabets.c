24ccb66279956497206cf00480cd7d5fde6390e7
newisdartagnan <newisboris@gmail.com>
2022-09-11 13:45:24 -0700

    rr

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,40 @@
+#include <stdlib.h>
+#include <ctype.h>
+#include <stdio.h>
+#include <time.h>
+
+/* more headers goes there */
+
+
+
+/* betty style doc for function main goes there */
+
+/**
+ * main - does nothing with void as arg
+ *
+ * Return: a null value to destroy or stop program
+ */
+
+int main(void)
+
+{
+/* your code goes there */
+
+	int x, y;
+	x = 'a';
+	while (x <= 'z')
+	{
+		putchar(x);
+		x++;
+	}
+	
+	y = 'A';
+	while (y <= 'Z')
+	{
+		putchar(y);
+		y++;
+	}
+	putchar('\n');
+	return (0);
+}
+
