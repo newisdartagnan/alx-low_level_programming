4a50bfddcec27930a952e3afec263ebaa7c8f8da
newisdartagnan <newisboris@gmail.com>
2022-09-12 11:21:35 -0700

    rr

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,40 @@
+#include <stdlib.h>
+#include <ctype.h>
+#include <stdio.h>
+#include <time.h>
+
+/* more headers goes there */
+
+
+
+/* betty style doc for function main goes there */
+
+/**
+ * main - does nothing with void as arg
+ *
+ * Return: a null value to destroy or stop program
+ */
+
+int main(void)
+
+{
+
+/* your code goes there */
+
+	int x;
+
+	for (x = 48; x < 58; x++)
+	{
+		putchar(x);
+		if (x != 57)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+
+	}
+	putchar('\n');
+	return (0);
+}
+
+
