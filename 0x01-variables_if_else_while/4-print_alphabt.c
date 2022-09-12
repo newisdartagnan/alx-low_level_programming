9505f51ca0e862709c623ea87f6a78d4c79f37a7
newisdartagnan <newisboris@gmail.com>
2022-09-12 10:32:07 -0700

    rr

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,36 @@
+#include <stdlib.h>
+#include <ctype.h>
+#include <stdio.h>
+#include <time.h>
+
+/* more headers goes there */
+
+
+
+/* betty style doc for function main goes there */
+
+/**
+ * main - does nothing with void as arg
+ *
+ * Return: a null value to destroy or stop program
+ */
+
+int main(void)
+
+{
+
+/* your code goes there */
+
+	int x;
+
+	for (x = 97; x < 123; x++)
+	{
+		if (x != 101 && x != 113)
+		{
+			putchar(x);
+		}
+	}
+	putchar('\n');
+	return (0);
+}
+
