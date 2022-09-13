c466fa6dcc4370958d308600a7b539b8891ab9bb
newisdartagnan <newisboris@gmail.com>
2022-09-12 20:15:15 -0700

    rr

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -14,11 +14,11 @@ int main(void)
 	long long int lliType;
 	char charType;
 
-	printf("Size of char: %zu byte(s)\n", sizeof(charType));
-	printf("Size of int: %zu byte(s)\n", sizeof(intType));
-	printf("Size of long int: %zu byte(s)\n", sizeof(liType));
-	printf("Size of long long int: %zu byte(s)\n", sizeof(lliType));
-	printf("Size of float: %zu byte(s)\n", sizeof(floatType));
+	printf("Size of a char: %zu byte(s)\n", sizeof(charType));
+	printf("Size of an int: %zu byte(s)\n", sizeof(intType));
+	printf("Size of a long int: %zu byte(s)\n", sizeof(liType));
+	printf("Size of a long long int: %zu byte(s)\n", sizeof(lliType));
+	printf("Size of a float: %zu byte(s)\n", sizeof(floatType));
 
 	return (0);
 }
