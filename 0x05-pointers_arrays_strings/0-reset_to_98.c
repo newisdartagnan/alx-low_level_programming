63b071b168a2c67732f874339c0c6e961dd609f0
newisdartagnan <newisboris@gmail.com>
2022-09-21 13:14:40 -0700

    voila

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <ctype.h>
+#include "main.h"
+/**
+ * reset_to_98 - Does nothing with void as arg
+ * @n : a pointer
+ * Description: Does nothing with void as arg
+ * Return: on success print caractere 1.
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,14 @@
+#ifndef PUTCHAR_H_
+#define PUTCHAR_H_
+
+int _putchar(char c);
+
+#endif
+
+#ifndef RESET_H_
+#define RESET_H_
+
+void reset_to_98(int *n);
+
+#endif
+
