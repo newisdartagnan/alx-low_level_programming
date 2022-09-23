16d088e426c9ad9673fdd434f8d244734bdee483
newisdartagnan <newisboris@gmail.com>
2022-09-23 16:37:28 -0700

    rr

---
+++ 0x06-pointers_arrays_strings/main.h
@@ -0,0 +1,14 @@
+#ifndef MAIN_H_
+#define MAIN_H_
+
+#include <string.h>
+#include <stdio.h>
+#include <ctype.h>
+#include <unistd.h>
+
+int _putchar(char c);
+char *_strcat(char *dest, char *src);
+
+#endif
+
+
