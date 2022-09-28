93efe1785d72aff9f0be7716a4fb5b843e2e3072
newisdartagnan <newisboris@gmail.com>
2022-09-28 05:31:11 -0700

    hello recursion

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * _puts_recursion - does something at the memory
+ * @s: a pointer
+ * Return: a specific pointer
+ */
+void _puts_recursion(char *s)
+{
+	puts(s);
+}
+
---
+++ 0x08-recursion/main.h
@@ -0,0 +1,14 @@
+#ifndef MAIN_H_
+#define MAIN_H_
+
+#include <string.h>
+#include <stdio.h>
+#include <ctype.h>
+#include <unistd.h>
+
+int _putchar(char c);
+void _puts_recursion(char *s);
+
+#endif
+
+
