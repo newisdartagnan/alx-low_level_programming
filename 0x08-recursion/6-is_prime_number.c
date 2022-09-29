857989daa5c8d44eefb15f37bdef2f03577c2767
newisdartagnan <newisboris@gmail.com>
2022-09-29 13:56:27 -0700

    rr

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -7,14 +7,16 @@
  */
 int is_prime_number(int n)
 {
-	int i;
+	int j;
 
-	if (n <= 1)
+	j = 2;
+	if (n <= 2)
+		return ((n == 2) ? 1 : 0);
+	if (n % j == 0)
 		return (0);
+	if (j * j > n)
+		return (1);
 
-	for (i = 2; i < n; i++)
-		if (n % i == 0)
-			return (0);
-
-	return (1);
+	/* j += 1; */
+	return (is_prime_number(j + 1));
 }
