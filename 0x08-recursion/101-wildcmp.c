19fe9975a5a06b12afa4623ff131d34fb8b34f8a
newisdartagnan <newisboris@gmail.com>
2022-09-29 18:01:38 -0700

    rr

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -11,7 +11,7 @@ int wildcmp(char *s1, char *s2)
 		return (1);
 	if (*s2 == '*')
 	{
-		while (*(s2 + 1) == '*')
+		if (*(s2 + 1) == '*')
 			s2++;
 	}
 	if (*s2 == '*' && *(s2 + 1) != '\0' && *s1 == '\0')
