579be6efb873498696bb9d1cfaf66a72612b4bf7
newisdartagnan <newisboris@gmail.com>
2022-09-29 18:36:11 -0700

    rr

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -6,18 +6,11 @@
  */
 char print_rev_recursion(char *s)
 {
-	int rem;
-	int sum = 0;
-
-	if (s != 0)
+	if (*s != '\0')
 	{
-		rem = s % 10;
-		sum = sum * 10 + rem;
-		print_rev_recursion(s / 10);
+		print_rev_recursion(s + 1);
 	}
-	else
-		return (sum);
-	return (sum);
+	return (*s);
 }
 
 /**
@@ -27,9 +20,7 @@ char print_rev_recursion(char *s)
  */
 int is_palindrome(char *s)
 {
-	if (strcmp(s, "") == 0)
-		return (1);
-	if (*s == print_rev_recursion(s))
+	if (*s == print_rev_recursion(s) || strcmp(s, "") == 0)
 	{
 		return (1);
 	}
