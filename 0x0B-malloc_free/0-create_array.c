db50010d79520b0e08aecd8e8c3582509b4edf8d
newisdartagnan <newisboris@gmail.com>
2022-10-05 19:21:59 -0700

    rr

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -10,7 +10,6 @@ char *create_array(unsigned int size, char c)
 	unsigned int i;
 	char *a = (char *)calloc(size, sizeof(char));
 
-	c = 'H';
 	if (c == '\0')
 		printf("%c\n", 'r');
 	if (size == 0)
