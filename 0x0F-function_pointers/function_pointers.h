2368697f5253492a113088df0809c11a4b14bd89
newisdartagnan <newisboris@gmail.com>
2022-10-12 16:40:15 -0700

    rr

---
+++ 0x0F-function_pointers/function_pointers.h
@@ -0,0 +1,13 @@
+#ifndef MAIN_H_
+#define MAIN_H_
+
+#include <string.h>
+#include <stdio.h>
+#include <ctype.h>
+#include <unistd.h>
+#include <math.h>
+
+int _putchar(char c);
+void print_name(char *name, void (*f)(char *));
+#endif
+
