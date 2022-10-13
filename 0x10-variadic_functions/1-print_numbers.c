e2f59a8fa7ba0c63a204f3260c68eeeb9c4faf22
newisdartagnan <newisboris@gmail.com>
2022-10-13 14:46:55 -0700

    rr

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,26 @@
+#include "variadic_functions.h"
+/**
+ * print_numbers - Short description
+ * @separator: a pointer
+ * @n: First member
+ * Description: Longer description
+ * Return: nothing
+ */
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	unsigned int i;
+	va_list p;
+
+	va_start(p, n);
+	if (separator == NULL)
+		printf("\n");
+	for (i = 0; i < n; i++)
+	{
+		printf("%d", va_arg(p, int));
+	if (i != n - 1)
+		printf("%s", separator);
+	else
+		printf("\n");
+	}
+	va_end(p);
+}
--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -9,5 +9,7 @@
 #include <stdarg.h>
 int _putchar(char c);
 int sum_them_all(const unsigned int n, ...);
+void print_numbers(const char *separator, const unsigned int n, ...);
+
 #endif
 
