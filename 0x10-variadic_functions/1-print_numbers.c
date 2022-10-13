4b74344b38cc1479210555df7957e4716c71d91e
newisdartagnan <newisboris@gmail.com>
2022-10-13 14:55:45 -0700

    rr

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -12,14 +12,13 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	va_list p;
 
 	va_start(p, n);
-	if (separator == NULL)
-		printf("\n");
 	for (i = 0; i < n; i++)
 	{
 		printf("%d", va_arg(p, int));
 	if (i != n - 1)
 		printf("%s", separator);
 	}
+	if (separator == NULL)
 	va_end(p);
 	printf("\n");
 }
