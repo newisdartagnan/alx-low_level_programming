44ce8d21c9165a5ab358c29e3f2be300cdd65305
newisdartagnan <newisboris@gmail.com>
2022-10-13 15:42:43 -0700

    rr

---
+++ 0x10-variadic_functions/2-print_strings.c
@@ -0,0 +1,27 @@
+#include "variadic_functions.h"
+/**
+ * print_strings - Short description
+ * @separator: a pointer
+ * @n: First member
+ * Description: Longer description
+ * Return: nothing
+ */
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	unsigned int i;
+	va_list p;
+
+	va_start(p, n);
+	for (i = 0; i < n; i++)
+	{
+		printf("%s", va_arg(p, char *));
+	if (i != n - 1)
+		printf("%s", separator);
+	}
+	if (separator == NULL)
+		printf("\n");
+	if (va_arg(p, char *) == NULL)
+		printf("nil");
+	va_end(p);
+	printf("\n");
+}
