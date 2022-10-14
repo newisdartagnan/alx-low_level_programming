1e345008f558e229c5a8393d2b977323bed23e19
newisdartagnan <newisboris@gmail.com>
2022-10-13 19:19:14 -0700

    rr

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -1,4 +1,5 @@
 #include "variadic_functions.h"
+#include <stdlib.h>
 /**
  * print_all - Short description
  * @format: First member
@@ -8,9 +9,32 @@
 void print_all(const char * const format, ...)
 {
 	va_list p;
+	char *t;
 
-	va_start(p);
-	va_arg(p, format);
-	va_end(p);
+	t = malloc(sizeof(char *));
+	va_start(p, format);
+	if (va_arg(p, char *) == NULL)
+		printf("nil");
+	while (*t != '\0')
+	{
+		if (*format == 'd')
+		{
+			printf("%d", va_arg(p, int));
+		}
+		else if (*format == 'c')
+		{
+			printf("%c", va_arg(p, int));
+		}
+		else if (*format == 'f')
+		{
+			printf("%f", va_arg(p, double));
+		}
+		else if (*format == 's')
+		{
+			printf("%s", va_arg(p, char *));
+		}
+		++t;
+	}
 	printf("\n");
+	va_end(p);
 }
