506066f2941f3bdcb57c403fb7a9a3dd757f523e
newisdartagnan <newisboris@gmail.com>
2022-10-14 19:36:10 -0700

    rr

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -15,7 +15,7 @@ int int_index(int *array, int size, int (*cmp)(int))
 	for (i = 0; i < size; i++)
 		if (cmp(*(array + i)))
 			return (i);
-	if (size <= 0)
+	if (size <= 0 || array == NULL || cmp == NULL)
 		return (-1);
 	return (-1);
 }
