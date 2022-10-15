2b064dbdd48971f5bcbc8f5e410902c4de9040d1
newisdartagnan <newisboris@gmail.com>
2022-10-14 18:35:01 -0700

    rr

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "function_pointers.h"
+
+/**
+ * array_iterator - prints a name as is
+ * @array: name of the person
+ * @size: size_t
+ * @action: function pointer
+ * Return: Nothing.
+ */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	long unsigned int i;
+
+	for (i = 0; i < size; i++)
+		action(*(array + i));
+}
--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,12 +1,12 @@
-#ifndef MAIN_H_
-#define MAIN_H_
+#ifndef FUNC_H_
+#define FUNC_H_
 
 #include <string.h>
 #include <stdio.h>
 #include <ctype.h>
 #include <unistd.h>
 #include <math.h>
-
+void array_iterator(int *array, size_t size, void (*action)(int));
 int _putchar(char c);
 void print_name(char *name, void (*f)(char *));
 #endif
