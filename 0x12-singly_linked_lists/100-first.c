1d704da18aa28cea93e1086cb11362ea3bd49df4
newisdartagnan <newisboris@gmail.com>
2022-10-20 22:18:42 -0700

    advanced

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+void before_main() __attribute__((constructor));
+/**
+ * before_main - before main
+ * Return: void
+ */
+void before_main()
+{
+	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+}
