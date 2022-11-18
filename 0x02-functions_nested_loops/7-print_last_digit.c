2d4b9cda108cbae1097922b69f159f2e439745cb
newisdartagnan <newisboris@gmail.com>
2022-11-18 00:51:25 -0800

    rrrr

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -3,7 +3,7 @@
 #include "main.h"
 /**
  * print_last_digit - Does nothing with void as arg
- * @x: a user-caractere
+ * @n: a user-caractere
  * Description: Does nothing with void as arg
  * Return: on success print caractere 1.
  */
