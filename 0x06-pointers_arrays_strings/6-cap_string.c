ab98b2bd17ca3692b26a1d89883e58141f3ab371
newisdartagnan <newisboris@gmail.com>
2022-11-18 02:51:25 -0800

    magic

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,22 +1,50 @@
 #include "main.h"
+
 /**
- * cap_string - Does nothing with void as arg
- * @s : a pointer
- * Description: Does nothing with void as arg
- * Return: on success print caractere 1.
- */
+* is_sep - check if a char is a seperator.
+* @s: the char to test.
+*
+* Return: 1 if seperator is found else 0
+*/
+int is_sep(char s)
+{
+	char sep[] = {'\t', '\n', ' ', ',', ';', '!',
+		      '.', '?', '\"', '(', ')', '{', '}'};
+	int i = 0;
+
+	while (sep[i] != '\0')
+	{
+		if (s == sep[i])
+			return (1);
+		i++;
+	}
+	return (0);
+}
+
+/**
+* cap_string - This function capitalizes all words of a string.
+* @s: sring to be processed.
+*
+* Return: pointer to the modified string.
+*/
 char *cap_string(char *s)
 {
-	int i;
+	int sep, i;
 
-	for (i = 0; s[i] != '\0'; i++)
+	sep = 1;
+	i = 0;
+	while (s[i] != '\0')
 	{
-		if (s[i - 1] == ' ' || s[i - 1] == '\n' || s[i - 1] == '\t')
+		if (sep == 1 && (s[i] >= 'a' && s[i] <= 'z'))
 		{
-			while (s[i - 1] == '\t')
-				s[i - 1] = ' ';
-			s[i] = toupper(s[i]);
+			s[i] -= 32;
+			sep = 0;
 		}
-	}
+		sep = is_sep(s[i]);
+		i++;
+}
 	return (s);
 }
+
+
+
