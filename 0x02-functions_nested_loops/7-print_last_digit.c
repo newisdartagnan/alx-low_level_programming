43708bb74e5c207555b59fd70f6fc439dd7aca60
newisdartagnan <newisboris@gmail.com>
2022-11-18 00:46:16 -0800

    all the rest

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -7,14 +7,13 @@
  * Description: Does nothing with void as arg
  * Return: on success print caractere 1.
  */
-
-int print_last_digit(int x)
+int print_last_digit(int n)
 {
-	int w, y, d;
-
-
-	d = x % 10;
-	w = d * 11;
-	y = ((unsigned int)(w < 0) ? -w : w);
-	return (y);
+	n %= 10;
+	if (n < 0)
+	{
+		n *= -1;
+	}
+	_putchar('0' + n);
+	return (n);
 }
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -58,6 +58,12 @@ int _abs(int);
 #define PRINTLAST_H_
 
 int print_last_digit(int);
+void jack_bauer(void);
+void times_table(void);
+int add(int, int);
+void print_to_98(int n);
+void print_times_table(int n);
+
 
 #endif
 
