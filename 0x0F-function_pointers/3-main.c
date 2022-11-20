83153da555a47593851d89de1648058206156ded
newisdartagnan <newisboris@gmail.com>
2022-11-20 01:39:44 -0800

    ouuff

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -1,12 +1,16 @@
 #include "function_pointers.h"
 #include <stdio.h>
 #include <stdlib.h>
-
+#define ARGV(X, S) (argv[X][0] == S)
 /**
- * main - check the code
+ * main - entry point, This fuction perform a calulaction base on 2 numbers and
+ * the given operator.
+ * @argc: number of agruments
+ * @argv: the string value of the arguments.
  *
- * Return: Always 0.
+ * Return: 0 (on Sucess)
  */
+
 int main(int argc, char *argv[])
 {
 	int a_int, b_int, result;
@@ -20,12 +24,11 @@ int main(int argc, char *argv[])
 	a_int = atoi(argv[1]);
 	b_int = atoi(argv[3]);
 	op = argv[2];
-	if (b_int == 0)
+	if ((ARGV(2, '/') || ARGV(2, '%')) && b_int == 0)
 	{
 		printf("Error\n");
 		exit(100);
 	}
-	
 	if (!(get_op_func(op)))
 	{
 		printf("Error\n");
